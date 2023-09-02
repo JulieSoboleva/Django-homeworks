128fee2167e45886116f3063c088a50ae4e0c87b
JulieSoboleva <ju.soboll@gmail.com>
2023-09-02 19:47:40 +0300

    Add phones catalog

--- 2.1-databases/work_with_database/phones/models.py
+++ 2.1-databases/work_with_database/phones/models.py
@@ -2,5 +2,14 @@ from django.db import models
 
 
 class Phone(models.Model):
-    # TODO: Добавьте требуемые поля
-    pass
+    name = models.CharField(max_length=100, null=False, default='Nokia')
+    price = models.DecimalField(default=0, max_digits=8, decimal_places=2)
+    image = models.URLField(max_length=200, null=True)
+    release_date = models.DateField(auto_now=False, auto_now_add=False,
+                                    null=True)
+    lte_exists = models.BooleanField(default=False)
+    slug = models.SlugField(max_length=100, unique=True, verbose_name='URL',
+                            default='Nokia')
+
+    def __str__(self):
+        return f"Phone(id={self.id}, name={self.name!r})"
--- 2.1-databases/work_with_database/phones/views.py
+++ 2.1-databases/work_with_database/phones/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render, redirect
 
+from phones.models import Phone
+
 
 def index(request):
     return redirect('catalog')
@@ -7,11 +9,24 @@ def index(request):
 
 def show_catalog(request):
     template = 'catalog.html'
-    context = {}
+    sort_param = request.GET.get('sort', '')
+    phones = Phone.objects.all()
+
+    if sort_param == 'max_price':
+        context = {'phones': phones.order_by('-price')}
+        return render(request, template, context)
+    elif sort_param == 'min_price':
+        context = {'phones': phones.order_by('price')}
+        return render(request, template, context)
+    elif sort_param == 'name':
+        context = {'phones': phones.order_by('name')}
+        return render(request, template, context)
+
+    context = {'phones': phones}
     return render(request, template, context)
 
 
 def show_product(request, slug):
     template = 'product.html'
-    context = {}
+    context = {'phone': Phone.objects.get(slug=slug)}
     return render(request, template, context)
