3c8cf8f34f80fb601fff140971b3481b4275c3f1
JulieSoboleva <ju.soboll@gmail.com>
2023-09-03 15:25:25 +0300

    Add books catalog

--- 2.1-databases/models_list_displaying/books/views.py
+++ 2.1-databases/models_list_displaying/books/views.py
@@ -1,7 +1,36 @@
-from django.shortcuts import render
+from django.core.paginator import Paginator
+from django.shortcuts import render, redirect
+
+from books.models import Book
+
+
+def index(request):
+    return redirect('books')
 
 
 def books_view(request):
     template = 'books/books_list.html'
-    context = {}
+    context = {'books': Book.objects.all()}
+    return render(request, template, context)
+
+
+def show_books_by_pub_date(request, pub_date):
+    template = 'books/books_list.html'
+    books = Book.objects.all()
+    paginator = Paginator(books, 10)
+    page_date = request.GET.get('pub_date')
+    page = paginator.get_page(page_date)
+    prev = Book.objects.filter(pub_date__lt=pub_date).values('pub_date').first()
+    if prev is not None:
+        prev = prev['pub_date'].strftime('%Y-%m-%d')
+    next = Book.objects.filter(pub_date__gt=pub_date).values('pub_date').first()
+    if next is not None:
+        next = next['pub_date'].strftime('%Y-%m-%d')
+    context = {
+        'page': page,
+        'pub_date': pub_date,
+        'books': Book.objects.filter(pub_date=pub_date),
+        'prev': prev,
+        'next': next,
+    }
     return render(request, template, context)
--- 2.1-databases/models_list_displaying/main/urls.py
+++ 2.1-databases/models_list_displaying/main/urls.py
@@ -14,11 +14,16 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, register_converter
 
-from books.views import books_view
+import books.views
+from books.converters import DateConverter
+
+register_converter(DateConverter, 'date')
 
 urlpatterns = [
-    path('', books_view, name='books'),
+    path('', books.views.index),
     path('admin/', admin.site.urls),
+    path('books/', books.views.books_view, name='books'),
+    path('books/<date:pub_date>/', books.views.show_books_by_pub_date),
 ]
